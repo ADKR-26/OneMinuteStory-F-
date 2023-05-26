03f14002925b743b9704909eb526982addc0f556
Aaditya Kumar <adityavishwakarma2011@gmail.com>
2023-05-26 23:02:45 +0530

    Tailwind CSS Setup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        <div
+            className="text-blue-400
+            text-6xl
+            sm: text-l
+            bg-black 
+            w-98
+            h-20"
+        >
+            One Minute Story
+        </div>
+    )
+}
+
+export default Header
