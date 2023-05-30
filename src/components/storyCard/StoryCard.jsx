3e33561625f1e324bab95afad7b9297e2963af9f
Aaditya Kumar <adityavishwakarma2011@gmail.com>
2023-05-30 21:03:57 +0530

    Added Delete option in the story card

--- src/components/storyCard/StoryCard.jsx
+++ src/components/storyCard/StoryCard.jsx
@@ -1,9 +1,21 @@
 import { Card } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
+import { deleteStoryData } from '../../store/action';
 
-function StoryCard({ storyData }) {
+function StoryCard({ storyData, id }) {
+
+    const dispatch = useDispatch();
+
+    const deleteStory = (id) => {
+        dispatch(deleteStoryData(id));
+        console.log("IDDDD", id);
+    }
+
     return (
         <div className="flex justify-center items-center">
             <Card
+                key = {id}
                 // className="border border-black-1100 rounded-lg"
                 style={{
                     display: 'flex',
@@ -14,6 +26,7 @@ function StoryCard({ storyData }) {
                     border: '2px solid rgba(150, 150, 150, 1)'
                 }}
             >
+                <DeleteOutlined className="absolute top-0 right-1 cursor-pointer" onClick={() => deleteStory(id)} />
                 <p className='p-10'> {storyData} </p>
             </Card>
         </div>
