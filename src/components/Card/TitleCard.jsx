febe35aca0fc0eb073765b2fbc3a72bf90c18e28
SANJU <sanjubodra1420@gmail.com>
2024-01-01 20:40:05 +0530

    code fixed

--- src/components/Card/TitleCard.jsx
+++ src/components/Card/TitleCard.jsx
@@ -1,90 +1,83 @@
 import { Button, Card } from "antd";
 import { NavLink } from "react-router-dom";
 import {
-    actionSetTitleId,
-    deleteStoryData,
-    getStoryData,
+  actionSetTitleId,
+  deleteStoryData,
+  getStoryData,
 } from "../../store/action";
 import { useDispatch, useSelector } from "react-redux";
 import { DeleteOutlined } from "@ant-design/icons";
 
-import './titleCard.scss';
+import "./titleCard.scss";
 
-function TitleCard({ titleData, id, email, author }) {
-    const currentUser = useSelector(
-        (state) => state?.oneMinuteStory?.currentUser?.data
-    );
+function TitleCard({ title, id, email, author }) {
+  const currentUser = useSelector(
+      (state) => state?.oneMinuteStory?.currentUser?.data
+  );
 
-    console.log(author);
+  const dispatch = useDispatch();
 
-    // console.log("EMAIL", email);
+  const setTitleId = () => {
+    dispatch(actionSetTitleId(id));
+  };
 
-    const dispatch = useDispatch();
+  const deleteStory = (id) => {
+    dispatch(deleteStoryData(id));
 
-    const setTitleId = () => {
-        dispatch(actionSetTitleId(id));
-        // console.log("DATA DELETED");
-    };
+    setTimeout(() => {
+      //TODO: Need to find a different way to rerender the data
+      dispatch(getStoryData());
+    }, 500);
+    // window.location.reload();          //! will reload / re-render when data is deleted not working
+  };
 
-    const deleteStory = (id) => {
-        dispatch(deleteStoryData(id));
-
-        setTimeout(() => {              //TODO: Need to find a different way to rerender the data
-            dispatch(getStoryData());
-        }, 500);
-        // window.location.reload();          //! will reload / re-render when data is deleted not working
-    };
-
-    return (
+  return (
+    <div className="flex justify-center">
+      {/* Current Stories */}
+      <Card
+        key={id}
+        // className="w-400"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: 900,
+          marginBottom: 10,
+          border: "2px solid rgba(150, 150, 150, 1)",
+          boxShadow: "10px 10px rgba(210, 210, 210, 1)",
+          // gap: "20px",
+          marginTop: "20px",
+        }}
+      >
+        {currentUser?.email === email && (
+          <DeleteOutlined
+            className="absolute top-0 right-1 cursor-pointer text-2xl text-red-500"
+            onClick={() => deleteStory(id)}
+          />
+        )}
+        <p className="p-10 text-2xl">
+          <label className="font-bold">Title:</label>{title}
+        </p>
         <div className="flex justify-center">
-            {/* Current Stories */}
-            <Card
-                key={id}
-                // className="w-400"
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    width: 900,
-                    marginBottom: 10,
-                    border: "2px solid rgba(150, 150, 150, 1)",
-                    boxShadow: "10px 10px rgba(210, 210, 210, 1)",
-                    // gap: "20px",
-                    marginTop: "20px",
-                }}
+          <Button
+            onClick={setTitleId}
+            className="text-xl h-12 bg-blue-900 text-white uppercase font-semibold py-2 rounded-xl shadow-xl"
+          >
+            <NavLink
+              to={{
+                pathname: "/story_details",
+              }}
             >
-                {currentUser?.email === email ? (
-                    <DeleteOutlined
-                        className="absolute top-0 right-1 cursor-pointer text-2xl text-red-500"
-                        onClick={() => deleteStory(id)}
-                    />
-                ) : (
-                    ""
-                )}
-                <p className="p-10 text-2xl">
-                    <label className="font-bold">Title:</label> {titleData}{" "}
-                </p>
-                <div className="flex justify-center">
-                    <Button
-                        onClick={setTitleId}
-                        className="text-xl h-12 bg-blue-900 text-white uppercase font-semibold py-2 rounded-xl shadow-xl"
-                    >
-                        <NavLink
-                            to={{
-                                pathname: "/story_details",
-                                // state: { key: demo }
-                            }}
-                        >
-                            View
-                        </NavLink>
-                    </Button>
-                </div>
-                <div className="flex justify-center text-lg font-semibold">
-                    <label htmlFor="">Author: {author} </label>
-                </div>
-            </Card>
+              View
+            </NavLink>
+          </Button>
         </div>
-    );
+        <div className="flex justify-center text-lg font-semibold">
+          <label htmlFor="">Author: {author} </label>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 export default TitleCard;
--- src/pages/storyFeed/StoryFeed.jsx
+++ src/pages/storyFeed/StoryFeed.jsx
@@ -1,40 +1,58 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import AddStory from "../addStory/AddStory";
 
 import "./storyFeed.scss";
+import { Switch } from "antd";
 
 function StoryFeed() {
-    const titleId = useSelector((state) => state?.oneMinuteStory?.titleId);
-    const storyData = useSelector((state) => state?.oneMinuteStory?.storyData);
+  const [paraOption, setOption] = useState(true);
+  const titleId = useSelector((state) => state?.oneMinuteStory?.titleId);
+  const storyData = useSelector((state) => state?.oneMinuteStory?.storyData);
 
-    const specificStory = storyData?.find((item) => item?._id === titleId);
+  const specificStory = storyData?.find((item) => item?._id === titleId);
 
-    console.log("Specific Story", specificStory);
+  const optionRender = (story) => {
+    if (paraOption) {
+      return (
+        <div className="storyFeed-paragraph">
+          {story?.map((item) => item.content).join(" ")}
+        </div>
+      );
+    } else {
+      return story.map((story) => (
+        <div key={story._id}>&nbsp;{story.content} </div>
+      ));
+    }
+  };
 
-    return (
-        <section id="storyFeed-jsx">
-            <div>
-                {specificStory ? (
-                    <div className="main-container">
-                        <div className="title-container">
-                            <label htmlFor=""> Title:&nbsp;</label>
-                            {specificStory.title.toUpperCase()}
-                        </div>
-                        <div className="story-container">
-                            {specificStory.story.map((story) => (
-                                <div key={story._id}>
-                                    &nbsp;{story.content}{" "}
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                ) : (
-                    <div>No story found</div>
-                )}
-                <AddStory titleData={specificStory.title} />
+  return (
+    <section id="storyFeed-jsx">
+      <div>
+        {specificStory ? (
+          <div className="main-container">
+            <div className="title-container">
+              <label htmlFor=""> Title:&nbsp;</label>
+              {specificStory.title.toUpperCase()}
             </div>
-        </section>
-    );
+            <span className="para-switch-container">
+              {paraOption ? "Paragraph" : "Block"} :
+              <Switch
+                className="para-switch"
+                onChange={(prev) => setOption(!prev)}
+              />
+            </span>
+            <div className="story-container">
+              {optionRender(specificStory.story)}
+            </div>
+          </div>
+        ) : (
+          <div>No story found</div>
+        )}
+        <AddStory titleData={specificStory.title} />
+      </div>
+    </section>
+  );
 }
 
 export default StoryFeed;
